abf133e4bfcd18e73c062136c59e6fe86ba33b6b
Alp <alp@Alper-MacBook-Pro.local>
2014-10-29 23:28:40 +0200

    ctq/main.py updated

--- ctq/Main.py
+++ ctq/Main.py
@@ -2,31 +2,50 @@ __author__ = 'alp'
 from PIL import Image
 from random import randint
 
-def setimage(path, pxcount):
+def create_image(path, pxcount):
+    """
+    gorselin random piksel degerlerini degistirerek yeni gorsel olusturur
+    pxcount: kac kez random piksel degistirme islemi yapiliyor
+    """
     img = Image.open(path, 'r')
     pixels = img.load()
-    for i in range(0, pxcount):
-        x = randint(0, img.size[0]-1)
-        y = randint(0, img.size[0]-1)
+    for i in range(pxcount):
+        x = randint(img.size[0]-1)
+        y = randint(img.size[0]-1)
         if pixels[x, y] == (0,0,0):
             pixels[x, y] = 255,255,255
         else:
             pixels[x, y] = 0,0,0
     return img
 
-def saveimage(start, stop, imgcount, label):
+def save_image(start, stop, imgcount, label):
+    """
+    degistirilmis gorseli kaydeder. label text dosyasi olusturur, kaydeder
+    """
     text = ""
-    for p in range(0,imgcount):
+    imgfile = select_file(label)
+    for p in range(imgcount):
         pxcnt = randint(start, stop)
-        imgcurrent = setimage(imgfile, pxcnt)
-        filename = "img_quad_train" + str(pxcnt) + "_" + str(p) + ".png"
-        text += "ctq/dataset/train/" + filename + " " + str(label) + "\n"
+        imgcurrent = create_image(imgfile, pxcnt)
+        filename = "img_" + str(label) + "_" + str(p) + "_" + str(pxcnt) + ".png"
+        text += filename + "\n"
         imgcurrent.save(filename)
-
     text_file = open(imgfile + "_label.txt", "w")
     text_file.write(text)
     text_file.close()
 
-imgfile = "quad.png"
+def select_file(imglabel):
+    """
+    etikete gore gorseli secer. (varsayilan: root path)
+    """
+    if imglabel == 0:
+        imgfile = "circle.png"
+    elif imglabel == 1:
+        imgfile = "tri.png"
+    elif imglabel == 2:
+        imgfile = "quad.png"
+    return  imgfile
+
+
 #labels: circle=0, tri=1, quad=2
-saveimage(100, 300, 2000, 2)
+save_image(100, 300, 2000, 0)
