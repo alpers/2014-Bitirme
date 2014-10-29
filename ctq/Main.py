d65bae12ea3f7dcfa44f5c0480965ef30a392c49
Alp <alp@Alper-MacBook-Pro.local>
2014-10-29 21:56:20 +0200

    images merged

--- ctq/Main.py
+++ ctq/Main.py
@@ -19,14 +19,14 @@ def saveimage(start, stop, imgcount, label):
     for p in range(0,imgcount):
         pxcnt = randint(start, stop)
         imgcurrent = setimage(imgfile, pxcnt)
-        filename = "img" + str(pxcnt) + "_" + str(p) + ".png"
-        text += "ctq/dataset/images/" + imgfile + "/" + filename + ", " + str(label) + "\n"
+        filename = "img_quad_train" + str(pxcnt) + "_" + str(p) + ".png"
+        text += "ctq/dataset/train/" + filename + " " + str(label) + "\n"
         imgcurrent.save(filename)
 
     text_file = open(imgfile + "_label.txt", "w")
     text_file.write(text)
     text_file.close()
 
-imgfile = "circle.png"
+imgfile = "quad.png"
 #labels: circle=0, tri=1, quad=2
-saveimage(100, 300, 500, 0)
+saveimage(100, 300, 2000, 2)
