588bdb5607d34033f5dbe23e45d1804f616134ca
Alp <alp@Alper-MacBook-Pro.local>
2014-10-29 16:39:08 +0200

    main.py updated

--- Main.py
+++ Main.py
@@ -19,16 +19,14 @@ def saveimage(start, stop, imgcount, label):
     for p in range(0,imgcount):
         pxcnt = randint(start, stop)
         imgcurrent = setimage(imgfile, pxcnt)
-        filename = "img_" + str(pxcnt) + "_" + str(randint(0, 1000)) + ".png"
-        text += "/images/" + imgfile + "/" + filename + ", " + str(label) + "\n"
+        filename = "img_" + str(pxcnt) + "_" + str(p) + ".png"
+        text += "images/" + imgfile + "/" + filename + ", " + str(label) + "\n"
         imgcurrent.save(filename)
 
     text_file = open(imgfile + "_label.txt", "w")
     text_file.write(text)
     text_file.close()
 
-imgfile = "circle.png"
+imgfile = "quad.png"
 #labels: circle=0, tri=1, quad=2
-
-saveimage(100, 200, 1000, 0)
-saveimage(200, 300, 1000, 0)
+saveimage(100, 300, 2000, 2)
