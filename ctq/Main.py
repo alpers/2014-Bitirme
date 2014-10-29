63fc5d0360cc40a62cc5e508e5f868b0cece4b0b
Alp <alp@Alper-MacBook-Pro.local>
2014-10-29 18:21:33 +0200

    test dataset added

--- ctq/Main.py
+++ ctq/Main.py
@@ -19,14 +19,14 @@ def saveimage(start, stop, imgcount, label):
     for p in range(0,imgcount):
         pxcnt = randint(start, stop)
         imgcurrent = setimage(imgfile, pxcnt)
-        filename = "img_" + str(pxcnt) + "_" + str(p) + ".png"
-        text += "images/" + imgfile + "/" + filename + ", " + str(label) + "\n"
+        filename = "img" + str(pxcnt) + "_" + str(p) + ".png"
+        text += "ctq/dataset/images/" + imgfile + "/" + filename + ", " + str(label) + "\n"
         imgcurrent.save(filename)
 
     text_file = open(imgfile + "_label.txt", "w")
     text_file.write(text)
     text_file.close()
 
-imgfile = "quad.png"
+imgfile = "circle.png"
 #labels: circle=0, tri=1, quad=2
-saveimage(100, 300, 2000, 2)
+saveimage(100, 300, 500, 0)
